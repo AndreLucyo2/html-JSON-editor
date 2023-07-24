964c53dce911740c6f7628f2a560c059380fed2f
AndreLucyo2 <andre.lucyo2@gmail.com>
2023-07-23 21:37:54 -0300

    versao 003

--- js/jsonFormat.js
+++ js/jsonFormat.js
@@ -1,15 +1,16 @@
-// Syntax highlighting for JSON keys, strings, and numbers using JavaScript
-const jsonBlocks = document.querySelectorAll('.json-block');
+        // Syntax highlighting for JSON keys, strings, and numbers using JavaScript
+        function highlightJSON(json) {
+            const highlightedJSON = json.replace(/(".*?":)/g, '<span class="json-key">$1</span>')
+                .replace(/(".*?")/g, '<span class="json-string">$1</span>')
+                .replace(/(\b\d+\b)/g, '<span class="json-number">$1</span>');
 
-jsonBlocks.forEach((block) => {
-    const lines = block.querySelectorAll('span');
-    lines.forEach((line) => {
-        if (line.classList.contains('json-key')) {
-            line.innerHTML = line.innerHTML.replace(/(".*?":)/g, '<span class="json-key">$1</span>');
-        } else if (line.classList.contains('json-string')) {
-            line.innerHTML = line.innerHTML.replace(/(".*?")/g, '<span class="json-string">$1</span>');
-        } else if (line.classList.contains('json-number')) {
-            line.innerHTML = line.innerHTML.replace(/(\b\d+\b)/g, '<span class="json-number">$1</span>');
+            return highlightedJSON;
         }
-    });
-});
+
+        function pasteJSON() {
+            const jsonInput = document.getElementById('jsonInput');
+            const jsonBlock = document.getElementById('jsonBlock');
+            const json = jsonInput.value;
+
+            jsonBlock.innerHTML = '<pre>' + highlightJSON(json) + '</pre>';
+        }
