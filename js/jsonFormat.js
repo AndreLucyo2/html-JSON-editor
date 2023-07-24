c5bcc79e59c5175090a54659d3319acbd8e56f55
AndreLucyo2 <andre.lucyo2@gmail.com>
2023-07-23 22:38:55 -0300

    versao 014 - reordenado as funções

--- js/jsonFormat.js
+++ js/jsonFormat.js
@@ -1,17 +1,30 @@
- // Syntax highlighting for JSON keys, braces, colon, boolean, strings, and numbers using JavaScript
- function highlightJSON(json) {
-    const formattedJSON = json
-        .replace(/"(\{|\})"/g, '<span class="json-braces">"$1"</span>') // {} - Chaves de abertura e fechamento
-        .replace(/":/g, '<span class="json-colon">":</span>') // : - Dois pontos
-        .replace(/:\s*(true|false)/g, '<span class="json-boolean">$1</span>') // Booleanos
-        .replace(/:\s*null/g, '<span class="json-null">$&</span>') // Nulos
-        .replace(/"(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:.\d{3}Z?|[+\-]\d{2}:\d{2})?)"/g, '<span class="json-string">"$1"</span>') // Strings (data)
-        .replace(/:\s*"([^"]*?)"/g, ': <span class="json-string">"$1"</span>') // Strings (delimitado por aspas)
-        .replace(/:\s*(-?\d+(\.\d+)?([eE][+-]?\d+)?)\b/g, ': <span class="json-number">$1</span>'); // Números
+/* Faz o processamento do JSON colado */
+function pasteJSON() {
+    const jsonInput = document.getElementById('jsonInput');
+    const jsonBlock = document.getElementById('jsonBlock');
+    const sortKeysCheckbox = document.getElementById('sortKeys');
+    const json = jsonInput.value;
 
-    return formattedJSON;
+    try {
+        let parsedJSON = JSON.parse(json);
+
+        if (sortKeysCheckbox.checked) {
+            parsedJSON = sortObjectKeys(parsedJSON);
+        }
+
+        const formattedJSON = formatJSON(parsedJSON);
+        jsonBlock.innerHTML = '<pre>' + formattedJSON + '</pre>';
+
+        const hiddenSection = document.querySelector('.hidden');
+        hiddenSection.style.display = 'none';
+
+        alert('JSON processado com sucesso!');
+    } catch (error) {
+        jsonBlock.innerHTML = '<pre>Erro: JSON inválido</pre>';
+    }
 }
 
+/* Executa ordenamento pela chave do json ordem alfabetica */
 function sortObjectKeys(obj) {
     if (typeof obj !== 'object' || obj === null) {
         return obj;
@@ -52,29 +65,18 @@ function formatJSON(obj, indentLevel = 0) {
     return formattedJSON;
 }
 
-function pasteJSON() {
-    const jsonInput = document.getElementById('jsonInput');
-    const jsonBlock = document.getElementById('jsonBlock');
-    const sortKeysCheckbox = document.getElementById('sortKeys');
-    const json = jsonInput.value;
+// Syntax highlighting for JSON keys, braces, colon, boolean, strings, and numbers using JavaScript
+function highlightJSON(json) {
+    const formattedJSON = json
+        .replace(/"(\{|\})"/g, '<span class="json-braces">"$1"</span>') // {} - Chaves de abertura e fechamento
+        .replace(/":/g, '<span class="json-colon">":</span>') // : - Dois pontos
+        .replace(/:\s*(true|false)/g, '<span class="json-boolean">$1</span>') // Booleanos
+        .replace(/:\s*null/g, '<span class="json-null">$&</span>') // Nulos
+        .replace(/"(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:.\d{3}Z?|[+\-]\d{2}:\d{2})?)"/g, '<span class="json-string">"$1"</span>') // Strings (data)
+        .replace(/:\s*"([^"]*?)"/g, ': <span class="json-string">"$1"</span>') // Strings (delimitado por aspas)
+        .replace(/:\s*(-?\d+(\.\d+)?([eE][+-]?\d+)?)\b/g, ': <span class="json-number">$1</span>'); // Números
 
-    try {
-        let parsedJSON = JSON.parse(json);
-
-        if (sortKeysCheckbox.checked) {
-            parsedJSON = sortObjectKeys(parsedJSON);
-        }
-
-        const formattedJSON = formatJSON(parsedJSON);
-        jsonBlock.innerHTML = '<pre>' + formattedJSON + '</pre>';
-
-        const hiddenSection = document.querySelector('.hidden');
-        hiddenSection.style.display = 'none';
-
-        alert('JSON processado com sucesso!');
-    } catch (error) {
-        jsonBlock.innerHTML = '<pre>Erro: JSON inválido</pre>';
-    }
+    return formattedJSON;
 }
 
 function clearJSONInput() {
