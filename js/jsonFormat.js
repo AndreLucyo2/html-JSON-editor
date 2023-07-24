6d9d3aa428906f0c36d76b431482b985cd16ac70
AndreLucyo2 <andre.lucyo2@gmail.com>
2023-07-24 00:07:29 -0300

    versao 015 refatoração sem IA

--- js/jsonFormat.js
+++ js/jsonFormat.js
@@ -18,7 +18,8 @@ function pasteJSON() {
         const hiddenSection = document.querySelector('.hidden');
         hiddenSection.style.display = 'none';
 
-        alert('JSON processado com sucesso!');
+        //alert('JSON processado com sucesso!');
+
     } catch (error) {
         jsonBlock.innerHTML = '<pre>Erro: JSON inválido</pre>';
     }
@@ -44,37 +45,42 @@ function sortObjectKeys(obj) {
     return sortedObj;
 }
 
-function formatJSON(obj, indentLevel = 0) {
+function formatJSON(obj, indentLevel = 1) {
+
     const indentSpaces = '  '.repeat(indentLevel);
-    let formattedJSON = '{\n';
+
+    //{
+    let formattedJSON = '<span class="json-braces">{</span>\n';
 
     for (const key in obj) {
-        formattedJSON += `${indentSpaces}<span class="json-key">"${key}"</span>: `;
+
+        formattedJSON += `${indentSpaces}<span class="json-key">"${key}"</span><span class="json-colon">:</span> `;
 
         if (typeof obj[key] === 'object') {
             formattedJSON += formatJSON(obj[key], indentLevel + 1);
+
         } else {
             formattedJSON += highlightJSON(JSON.stringify(obj[key]));
         }
 
-        formattedJSON += ',\n';
+        formattedJSON += '</span><span class="json-colon">,</span>\n';
     }
 
-    formattedJSON += `${'  '.repeat(indentLevel)}}`;
+    //}
+    formattedJSON += `${'  '.repeat(indentLevel)}<span class="json-braces">}</span>`;
 
     return formattedJSON;
 }
 
-// Syntax highlighting for JSON keys, braces, colon, boolean, strings, and numbers using JavaScript
-function highlightJSON(json) {
-    const formattedJSON = json
-        .replace(/"(\{|\})"/g, '<span class="json-braces">"$1"</span>') // {} - Chaves de abertura e fechamento
-        .replace(/":/g, '<span class="json-colon">":</span>') // : - Dois pontos
-        .replace(/:\s*(true|false)/g, '<span class="json-boolean">$1</span>') // Booleanos
+// Syntax highlighting boolean, strings, and numbers using JavaScript
+function highlightJSON(jsonValue) {
+    const formattedJSON = jsonValue
+        .replace('true', `<span class="json-boolean">${jsonValue}</span>`) // Booleanos
+        .replace('false', `<span class="json-boolean">${jsonValue}</span>`) // Booleanos
         .replace(/:\s*null/g, '<span class="json-null">$&</span>') // Nulos
         .replace(/"(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:.\d{3}Z?|[+\-]\d{2}:\d{2})?)"/g, '<span class="json-string">"$1"</span>') // Strings (data)
         .replace(/:\s*"([^"]*?)"/g, ': <span class="json-string">"$1"</span>') // Strings (delimitado por aspas)
-        .replace(/:\s*(-?\d+(\.\d+)?([eE][+-]?\d+)?)\b/g, ': <span class="json-number">$1</span>'); // Números
+        .replace(/(?<!")\b\d+\b(?!")/g, '<span class="json-number">$&</span>'); // Números (não entre aspas)
 
     return formattedJSON;
 }
