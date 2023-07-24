d2b95f30e05b4c6dc8e35822125fabbc9ea946dd
AndreLucyo2 <andre.lucyo2@gmail.com>
2023-07-23 21:33:09 -0300

    versao 001

--- js/jsonFormat.js
+++ js/jsonFormat.js
@@ -1,62 +1,13 @@
-// Syntax highlighting for JSON keys, strings, and numbers using JavaScript
-        function highlightJSON(json) {
-            const highlightedJSON = json.replace(/(".*?":)/g, '<span class="json-key">$1</span>')
-                .replace(/(".*?")/g, '<span class="json-string">$1</span>')
-                .replace(/(\b\d+\b)/g, '<span class="json-number">$1</span>');
+        // Syntax highlighting for keywords and strings using JavaScript
+        const codeBlocks = document.querySelectorAll('.code-block');
 
-            return highlightedJSON;
-        }
-
-        function sortObjectKeys(obj) {
-            if (typeof obj !== 'object' || obj === null) {
-                return obj;
-            }
-
-            if (Array.isArray(obj)) {
-                return obj.map(sortObjectKeys);
-            }
-
-            const sortedObj = {};
-            const keys = Object.keys(obj).sort();
-
-            for (const key of keys) {
-                sortedObj[key] = sortObjectKeys(obj[key]);
-            }
-
-            return sortedObj;
-        }
-
-        function formatJSON(obj, indentLevel = 0) {
-            const indentSpaces = '  '.repeat(indentLevel);
-            let formattedJSON = '';
-
-            for (const key in obj) {
-                if (typeof obj[key] === 'object') {
-                    formattedJSON += `${indentSpaces}<span class="json-key">"${key}"</span>: {\n${formatJSON(obj[key], indentLevel + 1)}${indentSpaces}}\n`;
-                } else {
-                    formattedJSON += `${indentSpaces}<span class="json-key">"${key}"</span>: ${highlightJSON(JSON.stringify(obj[key]))},\n`;
-                }
-            }
-
-            return formattedJSON;
-        }
-
-        function pasteJSON() {
-            const jsonInput = document.getElementById('jsonInput');
-            const jsonBlock = document.getElementById('jsonBlock');
-            const sortKeysCheckbox = document.getElementById('sortKeys');
-            const json = jsonInput.value;
-
-            try {
-                let parsedJSON = JSON.parse(json);
-
-                if (sortKeysCheckbox.checked) {
-                    parsedJSON = sortObjectKeys(parsedJSON);
-                }
-
-                const formattedJSON = formatJSON(parsedJSON);
-                jsonBlock.innerHTML = '<pre>' + formattedJSON + '</pre>';
-            } catch (error) {
-                jsonBlock.innerHTML = '<pre>Erro: JSON inválido</pre>';
-            }
-        }
+        codeBlocks.forEach((block) => {
+            const lines = block.querySelectorAll('p');
+            lines.forEach((line) => {
+                const text = line.innerHTML;
+                line.innerHTML = text.replace(/(\bconst\b|\bfunction\b|\breturn\b|\bif\b|\belse\b)\b/g, '<span class="keyword">$1</span>');
+                line.innerHTML = line.innerHTML.replace(/\/\/(.*)/g, '<span class="comment">//$1</span>');
+                line.innerHTML = line.innerHTML.replace(/"(.*?)"/g, '<span class="string">"$1"</span>');
+                line.innerHTML = line.innerHTML.replace(/(\d+)/g, '<span class="number">$1</span>');
+            });
+        });
