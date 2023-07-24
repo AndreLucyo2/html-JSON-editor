a3d0cf5d9c8d94fcc4fa43f28c9e69137665fa86
AndreLucyo2 <andre.lucyo2@gmail.com>
2023-07-23 21:40:37 -0300

    versao 004 - formatacao legal

--- js/jsonFormat.js
+++ js/jsonFormat.js
@@ -1,16 +1,37 @@
-        // Syntax highlighting for JSON keys, strings, and numbers using JavaScript
-        function highlightJSON(json) {
-            const highlightedJSON = json.replace(/(".*?":)/g, '<span class="json-key">$1</span>')
-                .replace(/(".*?")/g, '<span class="json-string">$1</span>')
-                .replace(/(\b\d+\b)/g, '<span class="json-number">$1</span>');
+// Syntax highlighting for JSON keys, strings, and numbers using JavaScript
+function highlightJSON(json) {
+    const highlightedJSON = json.replace(/(".*?":)/g, '<span class="json-key">$1</span>')
+        .replace(/(".*?")/g, '<span class="json-string">$1</span>')
+        .replace(/(\b\d+\b)/g, '<span class="json-number">$1</span>');
 
-            return highlightedJSON;
+    return highlightedJSON;
+}
+
+function formatJSON(obj, indentLevel = 0) {
+    const indentSpaces = '  '.repeat(indentLevel);
+    let formattedJSON = '';
+
+    for (const key in obj) {
+        if (typeof obj[key] === 'object') {
+            formattedJSON += `${indentSpaces}<span class="json-key">"${key}"</span>: {\n${formatJSON(obj[key], indentLevel + 1)}${indentSpaces}}\n`;
+        } else {
+            formattedJSON += `${indentSpaces}<span class="json-key">"${key}"</span>: ${highlightJSON(JSON.stringify(obj[key]))},\n`;
         }
+    }
 
-        function pasteJSON() {
-            const jsonInput = document.getElementById('jsonInput');
-            const jsonBlock = document.getElementById('jsonBlock');
-            const json = jsonInput.value;
+    return formattedJSON;
+}
 
-            jsonBlock.innerHTML = '<pre>' + highlightJSON(json) + '</pre>';
-        }
+function pasteJSON() {
+    const jsonInput = document.getElementById('jsonInput');
+    const jsonBlock = document.getElementById('jsonBlock');
+    const json = jsonInput.value;
+
+    try {
+        const parsedJSON = JSON.parse(json);
+        const formattedJSON = formatJSON(parsedJSON);
+        jsonBlock.innerHTML = '<pre>' + formattedJSON + '</pre>';
+    } catch (error) {
+        jsonBlock.innerHTML = '<pre>Erro: JSON inválido</pre>';
+    }
+}
